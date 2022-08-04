4acdb54626713e4cdfe22e523716d5af3af7a343
xxywla <xxywqrt@qq.com>
2022-08-04 21:44:37 +0800

    where choose when foreach include

--- mybatis_dynamicSQL/src/main/java/com/xxyw/mybatis/mapper/DynamicMapper.java
+++ mybatis_dynamicSQL/src/main/java/com/xxyw/mybatis/mapper/DynamicMapper.java
@@ -1,9 +1,18 @@
 package com.xxyw.mybatis.mapper;
 
 import com.xxyw.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface DynamicMapper {
     List<Emp> getEmpListByDynamic(Emp emp);
+
+    List<Emp> getEmpListByChoose(Emp emp);
+
+    void insertSomeEmp(@Param("empList") List<Emp> empList);
+
+    void deleteSomeEmp(@Param("empIds") int[] empIds);
+
+    List<Emp> getEmpList();
 }
--- mybatis_dynamicSQL/src/test/java/com/xxyw/mybatis/test/TestDynamic.java
+++ mybatis_dynamicSQL/src/test/java/com/xxyw/mybatis/test/TestDynamic.java
@@ -6,6 +6,7 @@ import com.xxyw.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestDynamic {
@@ -14,6 +15,39 @@ public class TestDynamic {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
         List<Emp> empList = mapper.getEmpListByDynamic(new Emp(null, "", 20, "男"));
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testChoose() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        List<Emp> empList = mapper.getEmpListByChoose(new Emp(null, "", null, "男"));
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsertSome() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        List<Emp> empList = new ArrayList<>();
+        empList.add(new Emp(null, "不知火", 18, "女"));
+        empList.add(new Emp(null, "不知火2", 18, "女"));
+        mapper.insertSomeEmp(empList);
+    }
+
+    @Test
+    public void testDeleteSome() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        mapper.deleteSomeEmp(new int[]{5, 6});
+    }
+
+    @Test
+    public void testInclude() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicMapper mapper = sqlSession.getMapper(DynamicMapper.class);
+        List<Emp> empList = mapper.getEmpList();
         empList.forEach(System.out::println);
     }
 }
